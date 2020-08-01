1d25503f957caa9607294512fbf0dcca96cf339a
Sushil Timilsina <developer.timilsina@gmail.com>
2020-08-01 22:13:23 +0545

    implementing delay and spinner

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
-import { promise } from 'protractor';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DishService {
+  constructor() {}
 
-  constructor() { }
-
-  getDishes(): Promise <Dish[]> {
-   return Promise.resolve(DISHES);
+  getDishes(): Promise<Dish[]> {
+    return new Promise((resolve) => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES), 2000);
+    });
   }
 
-  getDish(id: string): Promise <Dish> {
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+  getDish(id: string): Promise<Dish> {
+    return new Promise((resolve) => {
+      // Simulate server latency with 2 second delay
+      setTimeout(
+        () => resolve(DISHES.filter((dish) => dish.id === id)[0]),
+        2000
+      );
+    });
   }
 
-  getFeaturedDish(): Promise <Dish> {
-    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+  getFeaturedDish(): Promise<Dish> {
+    return new Promise((resolve) => {
+      // Simulate server latency with 2 second delay
+      setTimeout(
+        () => resolve(DISHES.filter((dish) => dish.featured)[0]),
+        2000
+      );
+    });
   }
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -3,21 +3,33 @@ import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LeaderService {
+  constructor() {}
 
-  constructor() { }
-
-  getLeaders(): Promise <Leader[]> {
-    return Promise.resolve(LEADERS);
+  getLeaders(): Promise<Leader[]> {
+    return new Promise((resolve) => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
-  getLeader(id: string): Promise <Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+  getLeader(id: string): Promise<Leader> {
+    return new Promise((resolve) => {
+      setTimeout(
+        () => resolve(LEADERS.filter((leader) => leader.id === id)[0]),
+        2000
+      );
+    });
   }
 
-  getFeaturedLeader(): Promise <Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+  getFeaturedLeader(): Promise<Leader> {
+    return new Promise((resolve) => {
+      setTimeout(
+        () => resolve(LEADERS.filter((leader) => leader.featured)[0]),
+        2000
+      );
+    });
   }
 }
