0e240a981fbd428b1999ce37404304d59cb7faf8
Sushil Timilsina <developer.timilsina@gmail.com>
2020-08-01 20:22:31 +0545

    Implementing peomises

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,23 +7,25 @@ import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 
 @Component({
-    selector: 'app-dishdetail',
-    templateUrl: './dishdetail.component.html',
-    styleUrls: ['./dishdetail.component.scss']
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
-
   dish: Dish;
 
-  constructor(private dishservice: DishService, private route: ActivatedRoute, private location: Location) { }
+  constructor(
+    private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.params.id;
-    this.dish = this.dishservice.getDish(id);
+    this.dishservice.getDish(id).then((dish) => (this.dish = dish));
   }
 
   goBack(): void {
     this.location.back();
   }
-
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
-
-
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -14,11 +11,11 @@ export class MenuComponent implements OnInit {
   dishes: Dish[];
   selectedDish: Dish;
 
-  constructor(private dishService: DishService) { }
+  constructor(private dishService: DishService) {}
 
   ngOnInit(): void {
-    this.dishes = this.dishService.getDishes();
-   }
+    this.dishService.getDishes().then((dishes) => (this.dishes = dishes));
+  }
 
   onSelect(dish: Dish) {
     this.selectedDish = dish;
